a8270bd878e0c46d7b0e9961cfc2dfcb30072f3d
Nakii0603 <naakii0603@gmail.com>
2023-10-11 12:21:30 +0800

    init

---
+++ index.js
@@ -0,0 +1,34 @@
+// let tsag = 55;
+// let honog = 24 ;
+// console.log((tsag / honog).toFixed(0));
+
+// let i = 0;
+// while(i <= 10){
+//    console.log (i) ;
+//    i=i+2;
+// }
+
+
+// for (i=1 ; i <= 100 ; i++){
+//     console.log(i)
+// }
+
+
+// let sum = 0;
+// for (let i = 0; i <= 10; i++) {
+//     if (i % 2  == 0)
+//         continue;
+//     sum = sum + i
+// } 
+// console.log(sum)
+
+
+let num = 2;
+let zerg = 5;
+
+let sum = 1;
+for(let i = 1; i <= zerg; i=i+1){
+    sum = sum * num;
+}
+console.log(sum);
+
