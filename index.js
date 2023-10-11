ad0c0ec2fef7effc656bae2fd3ef2ba74fffae79
Nakii0603 <naakiiznaakiiz@Naakiizs-MacBook-Air.local>
2023-10-11 14:42:06 +0800

    next

--- index.js
+++ index.js
@@ -19,16 +19,28 @@
 //     if (i % 2  == 0)
 //         continue;
 //     sum = sum + i
-// } 
+// }
 // console.log(sum)
 
 
-let num = 2;
-let zerg = 5;
+// let num = 2;
+// let zerg = 5;
 
-let sum = 1;
-for(let i = 1; i <= zerg; i=i+1){
-    sum = sum * num;
+// let sum = 1;
+// for(let i = 1; i <= zerg; i=i+1){
+//     sum = sum * num;
+// }
+// console.log(sum);
+
+
+// for (let a = 1; a <= 100; a = a + 1) {
+
+// }
+
+let sum = 0;
+for (let i = 0;i <= 10; i++) {
+    if (i % 2  == 0)
+        continue;
+    sum = sum + i
 }
-console.log(sum);
-
+console.log(sum)
