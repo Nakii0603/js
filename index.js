f1f53ca0de2157b0570563a874fe13fdb14b3ee7
Nakii0603 <naakii0603@gmail.com>
2023-10-12 12:38:02 +0800

    next-1

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Academy303
 // let tsag = 55;
 // let honog = 24 ;
 // console.log((tsag / honog).toFixed(0));
@@ -14,18 +15,8 @@
 // }
 
 
-// let sum = 0;
-// for (let i = 0; i <= 10; i++) {
-//     if (i % 2  == 0)
-//         continue;
-//     sum = sum + i
-// }
-// console.log(sum)
-
-
 // let num = 2;
 // let zerg = 5;
-
 // let sum = 1;
 // for(let i = 1; i <= zerg; i=i+1){
 //     sum = sum * num;
@@ -34,13 +25,76 @@
 
 
 // for (let a = 1; a <= 100; a = a + 1) {
-
 // }
 
-let sum = 0;
-for (let i = 0;i <= 10; i++) {
-    if (i % 2  == 0)
-        continue;
-    sum = sum + i
+
+// let sum = 0;
+// for (let i = 0;i <= 10; i++) {
+//     if (i % 2  == 0)
+//         continue;
+//     sum = sum + i
+// }
+// console.log(sum)
+
+// let sum = 0;
+// for (let i = 0;   i <= 10; i++ ){
+//     sum = sum + i;
+//     console.log(sum)
+// }
+
+
+// let num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// let i = 0;
+// while (i < num.length) {
+//     // console.log('Element at index ' + i + " is " + num[i])
+//     console.log(`Element at index ${i} is ${num[i]}`)
+//     i++
+// }
+
+
+// let blance = [1000 , 2000 , 2500 , 32442 , 5342];
+// let i = 0;
+// while (i < blance.length){
+//     console.log(blance[i] * 1.15)
+//     i++
+// }
+
+
+// let num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// let i = 0;
+
+
+// let array = [1, 2, 3, 4, 5]
+// let reversedArray = []
+// for(let i = array.length - 1; i >= 0; i--) {
+//   let valueAtIndex = array[i]
+//   reversedArray.push(valueAtIndex)
+// }
+// console.log(reversedArray)
+
+
+// while  (i < num.length) {
+//     // console.log('Element at index ' + i + " is " + num[i])
+//     console.log(`index ${i} is ${num[i]}`)
+//     i++
+// }
+
+let array = [1, 2, 3, 4, 5];
+let str = ""
+for (let i = 0; i < array.length; i++) {
+    for (let h = 0; h <= h + 1; h++) {
+        str = str +array [h]
+    }
+    console.log(str);
+    str = ""
 }
-console.log(sum)
+
+
+
+
+
+
+
+
+
+
